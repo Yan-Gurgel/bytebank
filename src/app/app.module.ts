0ce89da61a28950d9ce4bcc94adb9e142c9bfbc4
Yan-Gurgel <yan.gurgel@gmail.com>
2021-08-22 15:22:16 -0300

    Evento teste do formulário

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NovaTransferenciaComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent, NovaTransferenciaComponent],
